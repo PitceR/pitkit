27db0a505a9d6ab4556111d7de59cace22084f7d
Piotr <piotrekdch@interia.eu>
2017-10-06 19:45:49 +0200

    Add blank line between license header and package declaration

--- src/main/java/pl/pitkour/pitkit/inventory/BookInventory.java
+++ src/main/java/pl/pitkour/pitkit/inventory/BookInventory.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package pl.pitkour.pitkit.inventory;
 
 import java.util.Arrays;
--- src/main/java/pl/pitkour/pitkit/utility/Builder.java
+++ src/main/java/pl/pitkour/pitkit/utility/Builder.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package pl.pitkour.pitkit.utility;
 
 public interface Builder<T>
